311b84843b12f75c3804ec7159a2e5bf97e1b0cc
mahdi <mahdi.javan.gh2@gmail.com>
2020-02-03 21:33:41 +0330

    profile creation ( part 5 )

--- reddit/accounts/api/urls.py
+++ reddit/accounts/api/urls.py
@@ -8,10 +8,10 @@ router = routers.DefaultRouter()
 router.include_root_view = False
 
 router.register('register', UserView, basename='user_register')
+router.register('follow', FollowView, basename='follow')
 
 urlpatterns = router.urls + [
     path('login/', LoginView.as_view(), name='login'),
     path('update/', UserUpdateView.as_view(), name='update'),
-    path('follow/', FollowView.as_view(), name='followers'),
     path('profile/', ProfileView.as_view(), name='profile')
 ]
