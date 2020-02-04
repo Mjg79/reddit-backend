bc0c68fc555faace0aad879011c7def33e6ca20e
mahdi <mahdi.javan.gh2@gmail.com>
2020-02-04 21:57:27 +0330

    fixing avatar registering

--- reddit/accounts/api/views.py
+++ reddit/accounts/api/views.py
@@ -43,7 +43,7 @@ class UserUpdateView(generics.UpdateAPIView):
         data = request.data
         user = request.user
         if not data.get('email', None):
-            return Response(status=status.HTTP_403_FORBIDDEN)
+            raise exceptions.NotAcceptable('ez')
         user.first_name = data.get('first_name', '')
         user.last_name = data.get('last_name', '')
         user.email = data.get('email', '')
